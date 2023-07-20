f8c758c8d0c63cca9592a10505b79ff043ee5663
ZeNuW <teliatnikov.m@yandex.ru>
2023-07-21 09:31:27 +1000

    Внесены исправления, объединены контроллеры HitController и StatisticController, все зависимости из главного pom распределены в pom-ы модулей. Увеличино кол-во символов для uri в БД до 2048

--- statistic/statistic-service/src/main/java/ru/practicum/statisticservice/controller/StatisticController.java
+++ statistic/statistic-service/src/main/java/ru/practicum/statisticservice/controller/StatisticController.java
@@ -3,24 +3,24 @@ package ru.practicum.statisticservice.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.statisticdto.HitDto;
 import ru.practicum.statisticdto.ViewStats;
 import ru.practicum.statisticservice.service.StatisticService;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/stats")
 @Slf4j
 public class StatisticController {
     private final StatisticService statisticService;
 
-    @GetMapping
+    @GetMapping("/stats")
     public List<ViewStats> getStatistic(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
                                         @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                         @RequestParam(defaultValue = "false") Boolean unique,
@@ -28,5 +28,12 @@ public class StatisticController {
         log.info("Запрос к эндпоинту /stats - getStatistic с параметрами: start = " + start +
                 ", end = " + end + ", unique = " + unique + ", uris = " + uris);
         return statisticService.getStatistic(start, end, unique, uris);
+    }
+
+    @PostMapping("/hit")
+    public ResponseEntity<String> createHit(@RequestBody @Valid HitDto hitDto) {
+        log.info("Запрос к эндпоинту /hit - createHit с Hit: " + hitDto);
+        statisticService.createHit(hitDto);
+        return new ResponseEntity<>("Новый Hit сохранён", HttpStatus.CREATED);
     }
 }
