77728f5eed8939388507c89b2883239bd5fd42f0
ZeNuW <teliatnikov.m@yandex.ru>
2023-07-29 23:08:43 +1000

    refactor: MainApplication

--- main/src/main/java/ru/practicum/main/MainApplication.java
+++ main/src/main/java/ru/practicum/main/MainApplication.java
@@ -2,10 +2,8 @@ package ru.practicum.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan({"ru.practicum.statisticclient", "ru.practicum.main"})
+@SpringBootApplication(scanBasePackages = {"ru.practicum.statisticclient", "ru.practicum.main"})
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
